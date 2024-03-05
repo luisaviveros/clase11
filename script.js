4ac4a4855db8b85b3560ef01627e45f887b35d09
luisaviveros <luisaviveros73@gmail.com>
2024-03-05 08:06:11 -0500

    primeraclaseJS

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log("Hello world","luisa viveros");
+const nombre = "luisa";
+
+let apellido = "viveros";
+apellido = "viveros";
+
+const edad = 17.0;
+console.log( "Mi edad es", edad);
+
+const esMayorDeEdad = true;
+console.log("esMayor de Edad", esMayorDeEdad);
+
+const notaA = 5;
+const notaB = 3;
+
+const promedio = (notaA + notaB)/2;
+console.log("El promedio es", promedio);
+
+const NombreCom = nombre + " " + apellido;
+console.log("Mi nombre completo es", NombreCom);
+
+const TipoDeDato = typeof edad;
+console.log (TipoDeDato);
+
+const numero = parseInt("5");
+console.log(numero, typeof numero);
+
+const edadPorConsola= prompt("Ingresa tu edad");
+console.log(edadPorConsola, typeof edadPorConsola);
